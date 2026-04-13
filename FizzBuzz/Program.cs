a736f31ee01f08e832e71a5c52b0f815faa99f4b
ezkb <hi.ezkb@gmail.com>
2026-04-14 01:06:15 +0200

    looping input mode, not stable, need error handling and more

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -4,9 +4,28 @@ string welcomeMsg = "Welcome to Fizzbuzz!";
 string constraintsInfo = "FizzBuzz requires the following constraints: (1 ≤ X < Y ≤ N ≤ 100)";
 string inputInfo = "Please input according to pattern X Y N: ";
 
+int X = 0, Y = 0, N = 0;
+
+bool userInputValid = false;
+
 Console.WriteLine(welcomeMsg + "\n" + constraintsInfo);
-Console.Write(inputInfo);
 
-string[] userInput = Console.ReadLine().Split();
+while (!userInputValid)
+{
+    // if looped in and there is an error message show it, if empty it is the first time? 
+    string errorMessage = string.Empty;
 
-Console.WriteLine(userInput[0] + "_" + userInput[1] + "_" + userInput[2]);
+    Console.Write(inputInfo);
+    string[] userInput = Console.ReadLine().Split();
+
+    X = int.Parse(userInput[0]);
+    Y = int.Parse(userInput[1]);
+    N = int.Parse(userInput[2]);
+
+    Console.WriteLine(X + " " + Y + " " + N);
+}
+
+
+
+
+
