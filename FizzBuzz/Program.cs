2870631a634c0580c2206e50516dec202354f082
ezkb <hi.ezkb@gmail.com>
2026-04-14 00:44:31 +0200

    added reusable strings and save of inputed data

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,2 +1,12 @@
 ﻿// FizzBuzz 
-Console.WriteLine("Hello, World!");
+
+string welcomeMsg = "Welcome to Fizzbuzz!";
+string constraintsInfo = "FizzBuzz requires the following constraints: (1 ≤ X < Y ≤ N ≤ 100)";
+string inputInfo = "Please input according to pattern X Y N: ";
+
+Console.WriteLine(welcomeMsg + "\n" + constraintsInfo);
+Console.Write(inputInfo);
+
+string[] userInput = Console.ReadLine().Split();
+
+Console.WriteLine(userInput[0] + "_" + userInput[1] + "_" + userInput[2]);
