7eac655a383833f94be3ad63d73b866c30469f5c
ezkb <hi.ezkb@gmail.com>
2026-04-14 16:25:47 +0200

    added some checks but also considering changing the approach

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,26 +1,46 @@
 ﻿// FizzBuzz 
 
-string welcomeMsg = "Welcome to Fizzbuzz!";
-string constraintsInfo = "FizzBuzz requires the following constraints: (1 ≤ X < Y ≤ N ≤ 100)";
-string inputInfo = "Please input according to pattern X Y N: ";
+string welcomeText = "Welcome to Fizzbuzz!";
+string constraintsText = "FizzBuzz requires the following (1 ≤ X < Y ≤ N ≤ 100)";
+string inputText = "Please input according to pattern X Y N: ";
+string userInput;
 
 int X = 0, Y = 0, N = 0;
 
-bool userInputValid = false;
+Console.WriteLine(welcomeText + "\n" + constraintsText);
 
-Console.WriteLine(welcomeMsg + "\n" + constraintsInfo);
+bool userInputValid = false;
 
 while (!userInputValid)
 {
-    // if looped in and there is an error message show it, if empty it is the first time? 
+    Console.WriteLine("");
+    // if looped in and there is an error message show it
     string errorMessage = string.Empty;
 
-    Console.Write(inputInfo);
-    string[] userInput = Console.ReadLine().Split();
+    Console.Write(inputText);
+    userInput = Console.ReadLine();
 
-    X = int.Parse(userInput[0]);
-    Y = int.Parse(userInput[1]);
-    N = int.Parse(userInput[2]);
+    // checking if total value is empty
+    if (string.IsNullOrWhiteSpace(userInput))
+    {
+        Console.WriteLine("Input can not be empty, try again");
+        continue;
+    }
+
+    string[] parts = userInput.Split();
+
+    // checking if all 3 parts are there
+    if (parts.Length != 3 )
+    {
+        Console.WriteLine("Input 3 numbers exactly, try again");
+        continue;
+    }
+
+    X = int.Parse(parts[0]);
+    Y = int.Parse(parts[1]);
+    N = int.Parse(parts[2]);
 
     Console.WriteLine(X + " " + Y + " " + N);
+
+    userInputValid = true;
 }
