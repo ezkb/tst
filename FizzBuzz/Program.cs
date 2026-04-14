503c09406381361adc7399bf4a6d2685d7a36c31
ezkb <hi.ezkb@gmail.com>
2026-04-14 17:18:14 +0200

    changed approach for sanity check, checking if it is what i want instead of the opposite

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,8 +1,9 @@
-﻿// FizzBuzz 
+﻿
 
+// @todo do i need to declare any empty vars before use, how does c# handle this?
 string welcomeText = "Welcome to Fizzbuzz!";
 string constraintsText = "FizzBuzz requires the following (1 ≤ X < Y ≤ N ≤ 100)";
-string inputText = "Please input according to pattern X Y N: ";
+string inputText = "Please input according to pattern [X Y N]: ";
 string userInput;
 
 int X = 0, Y = 0, N = 0;
@@ -11,11 +12,16 @@ Console.WriteLine(welcomeText + "\n" + constraintsText);
 
 bool userInputValid = false;
 
+// Starting with input checks and sanitization
 while (!userInputValid)
 {
+    // just a visually preference to have a new line
+    // @todo - is there a better way?
     Console.WriteLine("");
+
+    // @todo might not be necessary
     // if looped in and there is an error message show it
-    string errorMessage = string.Empty;
+    // string errorMessage = string.Empty;
 
     Console.Write(inputText);
     userInput = Console.ReadLine();
@@ -23,24 +29,29 @@ while (!userInputValid)
     // checking if total value is empty
     if (string.IsNullOrWhiteSpace(userInput))
     {
-        Console.WriteLine("Input can not be empty, try again");
+        Console.WriteLine("Error: Input can not be empty, try again");
         continue;
     }
 
-    string[] parts = userInput.Split();
+    // spliting and sanitizing the input
+    string[] parts = userInput.Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
-    // checking if all 3 parts are there
-    if (parts.Length != 3 )
+    // checking if I have 3 entries, if each entry is a integer and if yes save value in correct var
+    bool isThreeNumbers = parts.Length == 3 &&
+                          int.TryParse(parts[0], out X) &&
+                          int.TryParse(parts[1], out Y) &&
+                          int.TryParse(parts[2], out N);
+
+    // if any of above checks are incorrect, generate a general error message
+    // instead of doing lots of different individual checks
+    if (!isThreeNumbers)
     {
-        Console.WriteLine("Input 3 numbers exactly, try again");
+        Console.WriteLine("Error: Please enter exactly 3 whole numbers separated by spaces.");
+        Console.WriteLine("Example: 3 5 20");
         continue;
     }
 
-    X = int.Parse(parts[0]);
-    Y = int.Parse(parts[1]);
-    N = int.Parse(parts[2]);
-
-    Console.WriteLine(X + " " + Y + " " + N);
+    Console.WriteLine("X: "+ X + "\n" + "Y: " + Y + "\n" + "N: " + N);
 
     userInputValid = true;
 }
