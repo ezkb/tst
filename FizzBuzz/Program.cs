e7a620bf94c6483a0605a0dacc9f5ba6996888a1
ezkb <hi.ezkb@gmail.com>
2026-04-14 17:36:00 +0200

    added a helper method for error messages and cleaned up

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,7 +1,4 @@
-﻿
-
-// @todo do i need to declare any empty vars before use, how does c# handle this?
-string welcomeText = "Welcome to Fizzbuzz!";
+﻿string welcomeText = "Welcome to Fizzbuzz!";
 string constraintsText = "FizzBuzz requires the following (1 ≤ X < Y ≤ N ≤ 100)";
 string inputText = "Please input according to pattern [X Y N]: ";
 string userInput;
@@ -15,21 +12,16 @@ bool userInputValid = false;
 // Starting with input checks and sanitization
 while (!userInputValid)
 {
-    // just a visually preference to have a new line
-    // @todo - is there a better way?
-    Console.WriteLine("");
-
-    // @todo might not be necessary
-    // if looped in and there is an error message show it
-    // string errorMessage = string.Empty;
+    // Visually preference to have a new line
+    Console.WriteLine();
 
     Console.Write(inputText);
     userInput = Console.ReadLine();
 
-    // checking if total value is empty
+    // checking if input is empty
     if (string.IsNullOrWhiteSpace(userInput))
     {
-        Console.WriteLine("Error: Input can not be empty, try again");
+        PrintError("Error: Input can not be empty, try again");
         continue;
     }
 
@@ -46,12 +38,19 @@ while (!userInputValid)
     // instead of doing lots of different individual checks
     if (!isThreeNumbers)
     {
-        Console.WriteLine("Error: Please enter exactly 3 whole numbers separated by spaces.");
-        Console.WriteLine("Example: 3 5 20");
+        PrintError("Error: Please enter exactly 3 whole numbers separated by spaces.");
+        PrintError("Example: 3 5 20");
         continue;
     }
 
     Console.WriteLine("X: "+ X + "\n" + "Y: " + Y + "\n" + "N: " + N);
 
     userInputValid = true;
+}
+//helper method for error messages
+static void PrintError(string message)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(message);
+    Console.ResetColor();
 }
